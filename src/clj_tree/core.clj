48496180602b42938f269830c6cfebedd9ab4a37
Frank Burnham <fra.burnham@gmail.com>
2015-04-14 14:06:12 -0400

    path-to-root, dfs, print-tree need tests written. some tests broken after refactor...

--- src/clj_tree/core.clj
+++ src/clj_tree/core.clj
@@ -1,40 +1,66 @@
 (ns clj-tree.core)
 
-;stores all the nodes in a vector
-;this should allow the prevention of circular storage
-;when storing a parent the data grows a whole mess with maps
-;so now the parent node will be an index in this vec, new nodes will be
-;conj'd to the node vec
-;this may need to be an array, so that it can be updated in place...
-;default tree size of 10 should be grown when the number of nodes
-;inserted is 70% full and cut down when it is less than 70% full
-(def node-array (atom (object-array 10)))
-(def num-nodes (atom 0))
-
-;rethink this, what about an object-array full of atoms?
+;don't want anyone else to mess with the mutables
+(defonce ^:private node-array (atom (object-array 10)))
+(defonce ^:private num-nodes (atom 0))
 
 (defstruct node :parent :children :data :index)
 
 (defn- update-node! [index key val]
   (swap! (aget @node-array index) assoc key val))
 
-(defn- grow-array! [arr]
+(defn- resize-array! [arr]
   (let [len (* 2 (alength arr))
         ret (object-array len)]
     (doseq [i (range (alength arr))]
       (aset ret i (aget arr i)))
     ret))
 
-(defn new-node! [parent children data]
+(defn node-by-index [index]
+  (if (nil? index)
+    nil
+    (deref (aget @node-array index))))
+
+(defn add-child! [parent-index child-index]
+  (let [{children :children} (node-by-index parent-index)]
+    (update-node! parent-index :children (conj children child-index))
+    (update-node! child-index :parent parent-index)))
+
+;index as nil for no parent. parent 0 is a valid parent
+(defn new-node! [parent-index child-indices data]
   (if (> @num-nodes (* 0.7 (alength @node-array)))
-    (swap! node-array grow-array!))
-  (let [i @num-nodes]
-    (swap! num-nodes inc)
-    (aset @node-array i (atom (struct node parent children data i)))))
+    (swap! node-array resize-array!))
+  (deref
+    (let [i @num-nodes]
+      (swap! num-nodes inc)
+      (aset @node-array i (atom (struct node parent-index child-indices data i)))
+      (if (not (nil? parent-index))
+        (add-child! parent-index i))
+      (aget @node-array i))))
 
-(defn add-child! [{children :children i :index}
-                  {child-index :index}]
-  (update-node! i :children (conj children child-index))
-  (update-node! child-index :parent i))
+(defn path-to-root [node]
+  (loop [n node
+         r [(:index n)]]
+    (let [parent (node-by-index (:parent n))]
+      (if (nil? parent)
+        r
+        (recur parent (conj r (:index parent)))))))
 
-(defn path-to-root [node])
+(defn dfs [root-index find-data]
+  (loop [node-stack [root-index]
+         r nil]
+    (if (empty? node-stack)
+      r
+      (let [node (node-by-index (first node-stack))
+            node-stack (rest node-stack)]
+        (if (= find-data (:data node))
+          (recur [] node)
+          (recur (concat (:children node) node-stack) nil))))))
+
+(defn print-tree [root-index]
+  (loop [node-stack [root-index]]
+    (if (not-empty node-stack)
+      (let [node (node-by-index (first node-stack))
+            node-stack (rest node-stack)]
+        (println "Child:" (:index node) "Parent:" (:parent node))
+        (recur (concat (:children node) node-stack))))))
